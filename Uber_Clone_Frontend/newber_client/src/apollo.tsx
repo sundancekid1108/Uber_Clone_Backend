5484a08c1b852eb1c713759d0d3651586c3fa7c3
Sundance <sundancekid1108@gmail.com>
2019-05-16 22:43:38 +0900

    FrontEnd, BackEnd정리

---
+++ Uber_Clone_Frontend/newber_client/src/apollo.tsx
@@ -0,0 +1,15 @@
+import ApolloClient, {Operation} from "apollo-boost";
+
+
+const client = new ApolloClient({
+    request: async (operation: Operation) => {
+        operation.setContext({
+            headers: {
+                "X-JWT": localStorage.getItem("jwt") || ""
+            }
+        });
+    },
+    uri: 'http://localhost:4000/graphql'
+});
+
+export default client;
